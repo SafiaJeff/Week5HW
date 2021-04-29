bc18a7f77862cc575992ab75e5b38e4eac904316
peanvtman <sgj29@drexel.edu>
2021-04-29 15:59:57 -0400

    added in alien energy

--- dev/js/blastOff.js
+++ dev/js/blastOff.js
@@ -2,10 +2,13 @@ import { gsap } from "gsap";
 
 export let blastOffTL = gsap.timeline();
 
+gsap.set("#alienEnergy path",{display:"none"});
+
 blastOffTL
-.to("#SpaceshipAlien",{duration:.15, rotation:-10, ease:"none"})
-.to("#SpaceshipAlien",{duration:.15, rotation:10, yoyo: true, repeat:10, ease:"none"})
+.to("#SpaceshipAlien",{duration:.15, rotation:-2, ease:"none"})
+.to("#SpaceshipAlien",{duration:.15, rotation:2, yoyo: true, repeat:10, ease:"none"})
 .to("#SpaceshipAlien",{duration:.15, rotation:0, ease:"none"})
-.to("#SpaceshipAlien",{duration:1, y: "-=350"}, "+=.75")
+.to("#SpaceshipAlien",{duration:3.5, y: "-=350", ease: "elastic.out(1, 0.3)"}, "+=.75")
+.to("#alienEnergy path",{duration: 3, alpha:1, stagger: 1, display:"block"})
 
 ;
