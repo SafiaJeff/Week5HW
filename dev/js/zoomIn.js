761fd9233b0a45e2bfdca1db63d52f6c4e82d282
peanvtman <sgj29@drexel.edu>
2021-04-29 12:44:39 -0400

    cow tail fail

--- dev/js/zoomIn.js
+++ dev/js/zoomIn.js
@@ -9,8 +9,8 @@ gsap.set("#Trees",{transformOrigin:"center center"});
 zoomTL
 .from("#Ground",{scale:20, duration:4, y:3500, ease:"power4.out"})
 .from("#Trees",{scale:20, duration:4, y:2500, ease:"power4.out"}, "-=3")
-.from("#LBush",{duration:1.5, x:-500},"Bushes", "-=3")
-.from("#RBush",{duration:1.5, x:1050}, "Bushes", "-=3")
+.from("#LBush",{duration:1.5, x:-500},"Bushes")
+.from("#RBush",{duration:1.5, x:1050}, "Bushes")
 
 //how also put early delay on bushes?
 
