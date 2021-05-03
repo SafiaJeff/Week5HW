5ee1c080fda1d95e6a5a1d88f533229db3ef7611
peanvtman <sgj29@drexel.edu>
2021-05-02 21:14:34 -0400

    moon winks & flies

--- dev/js/moonFace.js
+++ dev/js/moonFace.js
@@ -2,4 +2,11 @@ import { gsap } from "gsap";
 
 export let moonFaceTL = gsap.timeline();
 
-moonFaceTL.fromTo(".moon-things",{duration:2.5, display:"block", scale:0},{scale:1, duration: 1})
+gsap.set(".moon-things",{transformOrigin:"center center"});
+
+moonFaceTL.fromTo(".moon-things",{duration:1, display:"block", scale:0},{scale:1, duration: 1.5, ease: "elastic.out(1, 0.3)"})
+moonFaceTL.to("#left-eye", {scaleY:.15, duration:.18})
+moonFaceTL.to("#left-eye", {scaleY:1, duration:.18})
+moonFaceTL.to("#moon", {rotate:50, duration:.5}, "byemoon")
+moonFaceTL.to("#moon", {x:"+=1000", duration:1, ease: "power3.in"}, "byemoon")
+
